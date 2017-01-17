53a61b41b4dd15db23f9a5b7571e2e195e12bf53
zhen.cheng <chengzhen021@outlook.com>
2017-01-17 09:43:14 +0800

    Jing.Wang: update *.gitignore and finish linkedlist

--- Algorithm and Data Structure/Algorithm and Data Structure/Algorithm and Data Structure/Source.cpp
+++ Algorithm and Data Structure/Algorithm and Data Structure/Algorithm and Data Structure/Source.cpp
@@ -1,55 +1,124 @@
-// some Algorithm and Data Structure
-//
-
+﻿
 #include <iostream>
+#include <malloc.h>
+#include <time.h>
 #include <stdlib.h>
-#include <string>
-#include <math.h>
 #include <vector>
 
 using namespace std;
 
 
-//About: sort
-//Information:This is used to practice about "sort"
+int		SIZE = 50000;
 
-//bubble
+struct MyStruct * create();
+struct MyStruct * remove(struct MyStruct *link, int value);
+void  print(struct MyStruct *link);
+
+struct MyStruct
+{
+	int index;
+	MyStruct *next;
+};
 
 int main()
 {
-	int x, y;
-
-	for (int i = 0; i < 1000; i++)
+	struct MyStruct *my = create();
+	for (size_t i = 0; i < SIZE - 1; i++)
 	{
-		x = i;
-		for (int j = 0; j < 1000; j++)
-		{
-			y = j;
-			if (x * 10000 + y * 1000 == 305000)
-			{
-				cout << x << " " << y << endl;
-			}
-		}
+		my = remove(my, 3);
+		print(my);
 	}
 
 	system("pause");
 	return 0;
 }
 
+void print(struct MyStruct *link)
+{
+	struct MyStruct *head = link;
 
+	cout << link->index << " ";
+	link = link->next;
 
+	while (link->next != head->next)
+	{
+		cout << link->index << " ";
+		link = link->next;
+	}
 
+	cout << endl;
+}
+struct MyStruct * create()
+{
+	struct MyStruct *p1;
+	struct MyStruct *p2;
+	struct MyStruct *head;
 
+	head = NULL;
+	p2 = p1 = (struct MyStruct*)malloc(sizeof(struct MyStruct));
 
+	if (p1 == NULL)
+	{
+		EXIT_FAILURE;
+	}
+	else
+	{
+		for (size_t i = 1; i <= SIZE; i++)
+		{
+			p1 = (struct MyStruct*)malloc(sizeof(struct MyStruct));
+			p1->index = i;
+			if (head == NULL)
+			{
+				head = p1;
+				p2 = p1;
+			}
+			else
+			{
+				p2->next = p1;
+				p2 = p1;
+			}
+		}
 
+		p2->next = head;
+	}
 
+	return head;
+}
+struct MyStruct * remove(struct MyStruct *link, int value)
+{
+	static int index = 0;
+	struct MyStruct  *head1 = link;
+	struct MyStruct  *head = link;
+	struct MyStruct  *current = link;
+	struct MyStruct  *p1 = link;
+	if (p1 == NULL)
+	{
+		EXIT_FAILURE;
+	}
+	else
+	{
+		for (size_t i = 0; i < index + value; i++)
+		{
+			current = p1;
+			p1 = p1->next;
+		}
 
+		if (p1 == link)
+		{
+			link = p1->next;
+			while (head1->next != head)
+			{
+				head1 = head1->next;
+			}
 
+			head1->next = link;
+		}
+		else
+		{
+			current->next = p1->next;
+			index++;
+		}
+	}
 
-
-
-
-
-
-
-
+	return link;
+}
